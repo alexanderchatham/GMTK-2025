b2524e9db682a1d228dd56a1a3c8d1005635906b
alexanderchatham <chatham.alexander@gmail.com>
2025-07-30 18:38:45 -0400

    tilting character when moving dash upgraded
    second ladder

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -1,14 +1,22 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CharacterController : MonoBehaviour
 {
     public float speed = 5f;
+    public float jumpForce = 5f;
     public float MaxSpeed = 1f;
     public Vector3 moveDirection;
     public bool jumping = false;
     public bool dashed = false;
+    public bool dashing = false;
     public bool onLadder = false; // Added for ladder functionality
+    Coroutine dashCoroutine;
+    public Transform PlayerRenderer;
+    public List<GameObject> currentLadders = new List<GameObject>();
     int lastMoveIndicator = 0;
     public GameObject spawnPoint;
     Rigidbody2D rb;
@@ -20,6 +28,23 @@ public class CharacterController : MonoBehaviour
         {
             Debug.LogError("Rigidbody2D component not found on the character.");
         }
+    }
+    public void Update()
+    {
+
+        //transform.Translate(moveDirection * Time.deltaTime);
+        //Instead of translate use rb to apply movement
+        if (rb != null)
+        {
+            FindHorizontalVelocity();
+            if (onLadder)
+                LadderControl();
+        }
+        else
+        {
+            Debug.LogError("Rigidbody2D component is not assigned.");
+        }
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -33,12 +58,17 @@ public class CharacterController : MonoBehaviour
         }
         if (collision.gameObject.CompareTag("Ladder"))
         {
+            StopDash();
             rb.linearVelocity = Vector2.zero; // Stop the character when entering a ladder
             rb.gravityScale = 0; // Disable gravity while on the ladder
             jumping = false; // Reset jumping state when on a ladder
             dashed = false; // Reset dashed state when on a ladder
             onLadder = true; // Set onLadder to true when entering a ladder
             print("Entered ladder: " + collision.gameObject.name);
+            if(!currentLadders.Contains(collision.gameObject))
+            {
+                currentLadders.Add(collision.gameObject); // Add the ladder to the list if not already present
+            }
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -54,8 +84,53 @@ public class CharacterController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ladder"))
         {
             print("Exited ladder: " + collision.gameObject.name);
-            rb.gravityScale = 1; // Re-enable gravity when exiting the ladder
-            onLadder = false; // Set onLadder to false when exiting a ladder
+
+            if(currentLadders.Contains(collision.gameObject))
+            {
+                currentLadders.Remove(collision.gameObject); // Remove the ladder from the list when exiting
+            }
+            if(currentLadders.Count == 0)
+            {
+                rb.gravityScale = 1; // Re-enable gravity when exiting the ladder
+                onLadder = false; // Set onLadder to false when exiting a ladder
+            }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            LandOnGround();
+        }
+        if (collision.gameObject.CompareTag("MovingPlatform"))
+        {
+            // Attach to the moving platform
+            transform.SetParent(collision.transform);
+            LandOnGround();
+        }
+        if (collision.gameObject.CompareTag("Lava"))
+        {
+            StopDash();
+            // Reset jumping state when colliding with the ground
+            jumping = false;
+            dashed = false; // Reset dashed state when touching the ground
+            transform.position = spawnPoint.transform.position; // Reset position to spawn point
+        }
+    }
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            jumping = false; // Reset jumping state when colliding with the ground
+            dashed = false; // Reset dashed state when touching the ground
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("MovingPlatform"))
+        {
+            // Detach from the moving platform
+            transform.SetParent(null);
         }
     }
     public void OnInteract()
@@ -93,43 +168,6 @@ public class CharacterController : MonoBehaviour
             moveDirection = Vector3.zero;
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            LandOnGround();
-        }
-        if (collision.gameObject.CompareTag("MovingPlatform"))
-        {
-            // Attach to the moving platform
-            transform.SetParent(collision.transform);
-            LandOnGround();
-        }
-        if (collision.gameObject.CompareTag("Lava"))
-        {
-            // Reset jumping state when colliding with the ground
-            jumping = false;
-            dashed = false; // Reset dashed state when touching the ground
-            transform.position = spawnPoint.transform.position; // Reset position to spawn point
-        }
-    }
-    void OnCollisionStay(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            jumping = false; // Reset jumping state when colliding with the ground
-            dashed = false; // Reset dashed state when touching the ground
-        }
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("MovingPlatform"))
-        {
-            // Detach from the moving platform
-            transform.SetParent(null);
-        }
-    }
-
     private void LandOnGround()
     {
         // Reset jumping state when colliding with the ground
@@ -137,35 +175,46 @@ public class CharacterController : MonoBehaviour
         dashed = false; // Reset dashed state when touching the ground
         FindHorizontalVelocity();
     }
-
     public void OnJump()
     {
         if(jumping && dashed)
             return; // Prevent jumping if already in the air
         else if (jumping &&!dashed)
         {
-            dashed = true; // Allow jumping only if not already dashed
-            rb.AddForceX(5f * lastMoveIndicator, ForceMode2D.Impulse);
-        }
-        jumping = true;
-        rb.AddForceY(5f, ForceMode2D.Impulse);
-    }
-    public void Update()
-    {
-
-        //transform.Translate(moveDirection * Time.deltaTime);
-        //Instead of translate use rb to apply movement
-        if (rb != null)
-        {
-            FindHorizontalVelocity();
-            if (onLadder)
-                LadderControl();
+            dashCoroutine = StartCoroutine(Dash()); // Start the dash coroutine
         }
         else
         {
-            Debug.LogError("Rigidbody2D component is not assigned.");
+            jumping = true;
+            rb.AddForceY(jumpForce, ForceMode2D.Impulse);
         }
-
+    }
+    IEnumerator Dash()
+    {
+        if (dashed)
+            yield break; // Prevent multiple dashes
+        dashing = true; // Set dashing state to true
+        dashed = true;
+        PlayerRenderer.rotation = Quaternion.Euler(0,0, lastMoveIndicator == 1 ? -15 : 15); // Face the direction of the dash
+        rb.gravityScale = 0; // Disable gravity while dashing
+        rb.linearVelocityY = 0; // Reset vertical velocity to prevent jumping while dashing
+        rb.linearVelocityX = 30f * lastMoveIndicator;
+        yield return new WaitForSeconds(0.3f); // Dash duration
+        PlayerRenderer.rotation = Quaternion.Euler(0, 0, 0); // Reset rotation after dash
+        rb.linearVelocityX = rb.linearVelocityX * 0.5f; // Reduce speed after dash
+        rb.gravityScale = 1; // Re-enable gravity after dash
+        dashing = false; // Reset dashing state
+        dashCoroutine = null; // Clear the dash coroutine reference
+    }
+    private void StopDash()
+    {
+        if (dashCoroutine != null)
+        {
+            StopAllCoroutines(); // Stop any ongoing dash or jump when entering a ladder
+            dashing = false; // Reset dashing state when on a ladder
+            PlayerRenderer.rotation = Quaternion.Euler(0, 0, 0); // Reset rotation when entering a ladder
+            rb.gravityScale = 1; // Re-enable gravity after dash
+        }
     }
     private void LadderControl()
     {
@@ -191,7 +240,15 @@ public class CharacterController : MonoBehaviour
         {
             newHorizontalVelocity = -MaxSpeed;
         }
-        if (!dashed)
+        if (!dashing)
+        {
+            if(newHorizontalVelocity!= 0)
+                PlayerRenderer.rotation = Quaternion.Euler(0, 0, lastMoveIndicator == 1 ? -5 : 5); // Face the direction of the dash
+            else
+            {
+                PlayerRenderer.rotation = Quaternion.Euler(0, 0, 0); // Reset rotation when not moving
+            }
             rb.linearVelocityX = newHorizontalVelocity;
+        }
     }
 }
