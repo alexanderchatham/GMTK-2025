3d2816093b1e2ee7273ea7d74ce82a8aa2bca0d1
alexanderchatham <chatham.alexander@gmail.com>
2025-07-30 15:45:08 -0400

    First few deaths and secret crown unlockable!
    made everything prefabs, able to reverse moving platforms so they meet up nicely

--- Assets/MovingPlatform.cs
+++ Assets/MovingPlatform.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class MovingPlatform : MonoBehaviour
 {
     public Transform[] waypoints;
+    //inverse movement to make platforms move in the opposite direction
+    //this is useful for platforms that move in a loop or back and forth
+    public bool inverse;
     public float speed = 2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
@@ -14,6 +17,8 @@ public class MovingPlatform : MonoBehaviour
         if (waypoints.Length >= 2)
         {
             float t = Mathf.PingPong(Time.time * speed, 1f);
+            if(inverse)
+                t = 1f - t; // Inverse movement logic
             transform.position = Vector3.Lerp(waypoints[0].position, waypoints[1].position, t);
         }
         else
